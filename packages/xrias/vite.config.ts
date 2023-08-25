968f10289be982d9113eb63dde42e71b6ec56678
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-08-25 20:13:22 +0400

    :construction: Adjusts configs

---
+++ packages/xrias/vite.config.ts
@@ -0,0 +1,44 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  root: resolve(__dirname),
+  plugins: [
+    dts({
+      entryRoot: resolve(__dirname, 'src'),
+      tsconfigPath: resolve(__dirname, 'tsconfig.json'),
+    }),
+    tsconfigPaths(),
+  ],
+  define: {
+    'import.meta.vitest': 'undefined',
+    'import.meta.DEBUG': 'false',
+    '"$PROXY"': '',
+    '"$RAW"': '',
+    '"$SIZE"': '',
+    '"$EMPTY"': '',
+    '"$CACHEGETTERS"': '',
+  },
+  build: {
+    target: 'esnext',
+    outDir: resolve(__dirname, 'dist'),
+    lib: {
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es'],
+    },
+    minify: false,
+    rollupOptions: {
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: ({ name: fileName }) => {
+          return `${fileName}.js`;
+        },
+        sourcemap: true,
+      },
+    },
+  },
+});
