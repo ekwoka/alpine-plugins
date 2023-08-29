0c40977ca23491c5190e214ecdf2d65f9e94073c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-08-29 12:31:29 +0400

    :sparkles: Suppers Object fit cover (#10)
    * :sparkles: Suppers Object fit cover
    
    * :bookmark: Bumps xrias version

--- packages/xrias/src/resizeObserver.ts
+++ packages/xrias/src/resizeObserver.ts
@@ -1,15 +1,15 @@
-const debounceObserver = (
-  fn: (elements: Set<HTMLElement>, observer: ResizeObserver) => void,
+const debounceObserver = <T extends Element = HTMLElement>(
+  fn: (elements: Set<T>, observer: ResizeObserver) => void,
   delay: number,
 ): ResizeObserverCallback => {
-  let timer: number;
-  const allElements = new Set<HTMLElement>();
+  let timer: number | NodeJS.Timeout | null;
+  const allElements = new Set<T>();
   return function (
     entries: ResizeObserverEntry[],
     observer: ResizeObserver,
   ): void {
     if (timer) clearTimeout(timer);
-    entries.forEach(({ target }) => allElements.add(target as HTMLElement));
+    entries.forEach(({ target }) => allElements.add(target as T));
     timer = setTimeout(() => {
       fn(allElements, observer);
       timer = null;
@@ -17,18 +17,31 @@ const debounceObserver = (
   };
 };
 
-const observerCB = (els: Set<HTMLElement>) => {
+const observerCB = (els: Set<HTMLImageElement>) => {
   els.forEach((el) => resize(el));
+  els.clear();
 };
 
-const resize = (el: HTMLElement) => {
-  let sizes = el.offsetWidth;
-  let parent = el.parentNode as HTMLElement;
-  while (sizes < 100 && parent) {
-    sizes = parent.offsetWidth;
+const resize = (el: HTMLImageElement) => {
+  const setSize = (width: number) =>
+    el.setAttribute('sizes', (width | 0) + 'px');
+  let width = 0;
+  let height = 0;
+  let parent: HTMLElement | null = el;
+  while (width < 100 && parent) {
+    width = parent.offsetWidth;
+    height = parent.offsetHeight;
     parent = parent.parentNode as HTMLElement;
   }
-  el.setAttribute('sizes', sizes + 'px');
+  const objectFit = getComputedStyle(el).objectFit;
+  if (objectFit !== 'cover') return setSize(width);
+  const imageRatio =
+    Number(el.getAttribute('width')) / Number(el.getAttribute('height'));
+  if (!imageRatio) return setSize(width);
+  const displayedRatio = width / height;
+  if (displayedRatio >= imageRatio + 0.1) return setSize(width);
+  const newWidth = height * imageRatio;
+  setSize(newWidth);
 };
 
 export const observer = new ResizeObserver(debounceObserver(observerCB, 800));
